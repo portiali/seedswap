5351c5d25a54449b9a22c7fcfd12852ad77642fe
portialii89@gmail.com <portiali@dhcp-10-105-254-103.wireless.northwestern.private>
2025-04-05 18:27:49 -0500

    added auth

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,8 @@
+
+
+module.exports = (req, res, next) => {
+  if (!req.oidc.isAuthenticated()) {
+    return res.status(401).json({ message: 'Not logged in' });
+  }
+  next();
+};
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,21 @@
 // mongoose schema for user
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  auth0Id: { type: String, required: true, unique: true },
+  name: String,
+  email: { type: String, required: true, unique: true },
+  location: {
+    latitude: Number,
+    longitude: Number,
+  },
+  ownedSeeds: [
+    {
+      seedId: mongoose.Schema.Types.ObjectId,
+      quantity: Number
+    }
+  ],
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
