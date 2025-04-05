a695273e00b574a93495feb42dd420379a94cbf4
portialii89@gmail.com <portiali@dhcp-10-105-254-103.wireless.northwestern.private>
2025-04-05 17:40:54 -0500

    added env and gitignore

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 // import Signup from './Signup';
 // import Login from './Login';
